6dc02c1e429b51bb754de388e92a91a3e6d22032
qaotyk <mayniakgd@gmail.com>
2021-01-09 09:25:04 +0000

    clone.sh: Initial script     This script is designed to facilitate clone for the repository
    Signed-off-by: qaotyk <mayniakgd@gmail.com>

---
+++ clone.sh
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2021 qaøtyk <mayniakgd@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Command List
+CLONE=  "git clone -b"
+GITHUB= "https://github.com"
+GITLAB= "https:/gitlab.com"
+
+# Motorola Paths
+DEVICE= "device/motorola"
+COMMON= "device/motorola"
+VENDOR= "vendor/motorola"
+KERNEL= "kernel/motorola"
+MOTCAMERA="packages/apps/MotCamera2"
+
