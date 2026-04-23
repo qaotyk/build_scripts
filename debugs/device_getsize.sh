a8673839d88ee5a046aba753e1c5c611099e3059
qaotyk <mayniakgd@gmail.com>
2026-04-22 20:05:27 -0600

    debugs/adb: Add get_size utility
    *Add device_getsize.sh script for ADB utilities check
    
    Signed-off-by: qaotyk <mayniakgd@gmail.com>

---
+++ debugs/device_getsize.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Android Development Environment Tools v0.1130426
+# Powered by qaotyk <mayniakgd@gmail.com>
+#
+# Script to help set up an Android Environment on devices running Ubuntu
+# 
+# Copyright (c) 2026 qaotyk. All rights reserved.
+# This script is licensed under the MIT License.
+#
+
+# Start check ADB Utilities
+include get_adb_info.sh
+
+START_ADB
